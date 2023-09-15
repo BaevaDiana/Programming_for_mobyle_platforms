46c34cac6102947b143b38dcd373b16c6aac7832
pluxurypinkphloydrockstar <thedianab15@gmail.com>
2023-09-15 20:53:40 +0300

    ...

--- LW2/src/MyDate.java
+++ LW2/src/MyDate.java
@@ -7,16 +7,16 @@ public class MyDate {
         year = y;
     }
 
-//    @Override
-//    public boolean equals(Object o){
-//        boolean result = false;
-//        if (o != null && o instanceof MyDate){
-//            MyDate d = (MyDate)o;
-//            if((day == d.day) && (month == d.month) && (year == d.year)){
-//                result = true;
-//            }
-//        }
-//        return result;
-//    }
+    @Override
+    public boolean equals(Object o){
+        boolean result = false;
+        if (o != null && o instanceof MyDate){
+            MyDate d = (MyDate)o;
+            if((day == d.day) && (month == d.month) && (year == d.year)){
+                result = true;
+            }
+        }
+        return result;
+    }
 
 }
